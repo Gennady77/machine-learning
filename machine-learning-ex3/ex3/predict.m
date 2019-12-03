139f3be58b3c6c76c8ba260862ea44be5fda610a
Gennadii Kondratev <gipper2006@gmail.com>
2019-12-03 08:46:14 +0300

    Neural Networks

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,6 +21,15 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m,1) X];
+z2 = Theta1 * X';
+a2 = sigmoid(z2);
+
+a2 = [ones(1, size(a2, 2)); a2];
+z3 = Theta2 * a2;
+a3 = sigmoid(z3');
+
+[x, p] = max(a3, [], 2);
 
 
 
